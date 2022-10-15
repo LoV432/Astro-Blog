0a68c5e7d1567f2c8ab09fd4b7f9a218147e0643
LoV432 <monib619@gmail.com>
2022-10-15 21:57:55 +0500

    clean up and update deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	daisyui: {
+		themes: ["dark"],
+	  },
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui")
+	],
 }
