2de863b9c073c7450813f5afd9be06d073742ac2
LoV432 <monib619@gmail.com>
2022-10-16 21:55:46 +0500

    example config

---
+++ src/example.config.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Blog';
+export const SITE_DESCRIPTION = 'Welcome to my blog!';
+export const API = 'http://localhost:1337';
