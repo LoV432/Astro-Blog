642cb275545b7a4e1ddeff5198b09b84bee439d4
LoV432 <monib619@gmail.com>
2022-10-20 15:54:51 +0500

    move copyright name to config.ts

--- src/example.config.ts
+++ src/example.config.ts
@@ -1,6 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
+// Rename this file to "config.ts"
 
 export const SITE_TITLE = 'Blog';
+export const COPYRIGHT_NAME = 'XYZ';
 export const SITE_DESCRIPTION = 'Welcome to my blog!';
 export const API = 'http://localhost:1337';
