fa723515a470cbdb6583e405e8de3adc9e49058a
LoV432 <monib619@gmail.com>
2022-12-18 16:42:45 +0500

    change font of heading and some other styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
 	  },
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'rubik': ['"Rubik Vinyl"'],
+			  },
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
