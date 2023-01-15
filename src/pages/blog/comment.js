cfdb1edf1a153e3612cb6df5f6a13b8daa8a1309
LoV432 <monib619@gmail.com>
2023-01-15 16:01:33 +0500

    add cloudflare captcha

--- src/pages/blog/comment.js
+++ src/pages/blog/comment.js
@@ -1,5 +1,5 @@
 import DOMPurify from 'isomorphic-dompurify';
-import { API } from '../../config';
+import { API, CLOUDFLARE_SECRET_KEY } from '../../config';
 export async function post({ request, clientAddress }) {
 	let commenterIP;
 	if (request.headers.get('x-real-ip') != null) {
@@ -12,6 +12,11 @@ export async function post({ request, clientAddress }) {
 	let comment = data.comment.replace(/<[^>]+>/g, ''); // Strip all html tags
 	comment = comment.replace(/["]/g, '\\$&').replace(/\n/g, '\\n'); // Escape all "" and spaces
 	const sanitizedComment = DOMPurify.sanitize(comment); // Run it thru DOMPurify just to be sure
+	if (!(await validateCloudflare(CLOUDFLARE_SECRET_KEY, data.cloudflaretoken))) {
+		return new Response('{"response": "failed"}', {
+			status: 401
+		});
+	}
 	const options = {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
@@ -31,4 +36,15 @@ export async function post({ request, clientAddress }) {
 			status: 200
 		});
 	}
+}
+
+async function validateCloudflare(secretKey, token) {
+	const options = {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: '{"secret":"' + secretKey + '","response":"' + token + '"}'
+	};
+	let response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', options);
+	response = await response.json();
+	return response.success;
 }
