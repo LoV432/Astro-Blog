549746e62790fb6e3c5bef30ded8fb549047bd23
LoV432 <monib619@gmail.com>
2023-02-05 14:51:22 +0500

    load comments from client side

--- src/pages/blog/comment.js
+++ src/pages/blog/comment.js
@@ -10,9 +10,9 @@ export async function post({ request, clientAddress }) {
 		commenterIP = clientAddress;
 	}
 	const data = await request.json();
-	let comment = data.comment.replace(/<[^>]+>/g, ''); // Strip all html tags
-	comment = comment.replace(/["]/g, '\\$&').replace(/\n/g, '\\n'); // Escape all "" and spaces
-	const sanitizedComment = DOMPurify.sanitize(comment); // Run it thru DOMPurify just to be sure
+	const sanitizedComment = sanitize(data.comment);
+	const sanitizedEmail = sanitize(data.email);
+	const sanitizedName = sanitize(data.name);
 	if (!(await validateCloudflare(CLOUDFLARE_SECRET_KEY, data.cloudflaretoken))) {
 		return new Response('{"response": "failed"}', {
 			status: 401
@@ -21,7 +21,7 @@ export async function post({ request, clientAddress }) {
 	const options = {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
-		body: '{"author":{"id":"' + commenterIP + '","name":"' + data.name + '","email":"' + data.email + '","avatar":"' + avatar + '"},"content":"' + sanitizedComment + '"}'
+		body: '{"author":{"id":"' + commenterIP + '","name":"' + sanitizedName + '","email":"' + sanitizedEmail + '","avatar":"' + avatar + '"},"content":"' + sanitizedComment + '"}'
 	};
 	let response = await fetch(API + '/api/comments/api::post.post:' + data.post_id, options);
 	let status = response.status;
@@ -48,4 +48,10 @@ async function validateCloudflare(secretKey, token) {
 	let response = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', options);
 	response = await response.json();
 	return response.success;
+}
+
+function sanitize(value) {
+	value = value.replace(/<[^>]+>/g, ''); // Strip all html tags
+	value = value.replace(/["]/g, '\\$&').replace(/\n/g, '\\n'); // Escape all "" and spaces
+	return DOMPurify.sanitize(value); // Run it thru DOMPurify just to be sure
 }
