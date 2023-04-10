33e4f5ea66c95e4044fdcb2efcbf5d84df69b03a
LoV432 <monib619@gmail.com>
2023-04-10 16:36:48 +0500

    inline css and compress files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,32 @@
 import { defineConfig } from 'astro/config';
 import { BASE_URL } from './src/config';
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import node from '@astrojs/node';
 
 // https://astro.build/config
 import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
+import critters from "astro-critters";
+
+// https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-	site: `https://${BASE_URL}`,
-	integrations: [tailwind({ config: { applyBaseStyles: false } }), prefetch()],
-	output: 'server',
-	adapter: node({
-		mode: 'standalone'
-	})
+  site: `https://${BASE_URL}`,
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  }), prefetch(), critters({
+    logger: 0
+  }), compress({
+    html: false
+  })],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
